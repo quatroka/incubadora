1762450fd61234bbe83da921344553ff1b3e40a9
leomonteiro <monteiro.leonardosantos@gmail.com>
2015-12-20 00:13:51 -0200

    arquivos iniciais

---
+++ cardapio/README.md
@@ -0,0 +1,3 @@
+# Depencias
+
+* Materilialize 
---
+++ cardapio/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Cardápio</title>
+</head>
+
+<body>
+    <div class="container"><!--CONTAINER-->
+        <div class="row"><!--ROW-->
+            <div class="col s6 push-s3"><!--GRID-->
+                <ul class="collection">
+                    <li class="collection-item avatar">
+                        <img src="img/coxinha.jpg" alt="" class="circle">
+                        <span class="title">Coxinha</span>
+                        <p>Sabor: Frango<br>
+                            Uma deliciosa coxinha de frango.
+                        </p>
+                        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+                    </li>
+                    <li class="collection-item avatar">
+                        <i class="material-icons circle">folder</i>
+                        <span class="title">Coxinha</span>
+                        <p>First Line <br>
+                            Second Line
+                        </p>
+                        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+                    </li>
+                    <li class="collection-item avatar">
+                        <i class="material-icons circle green">insert_chart</i>
+                        <span class="title">Title</span>
+                        <p>First Line <br>
+                            Second Line
+                        </p>
+                        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+                    </li>
+                    <li class="collection-item avatar">
+                        <i class="material-icons circle red">play_arrow</i>
+                        <span class="title">Title</span>
+                        <p>First Line <br>
+                            Second Line
+                        </p>
+                        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+                    </li>
+                </ul>
+            </div><!--GRID-->
+        </div><!--ROW -->
+    </div><!--CONTAINER-->
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
